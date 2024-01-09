9928d82fb5a288942eb3c560acd24d99d0ee80c9
Kareem mohamed <info.kareemmohamed@gmail.com>
2024-01-09 02:01:49 +0200

    first initial step to Project structure division

---
+++ lib/features/screens/HomeScreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text('Hi Team',style: TextStyle(fontSize: 35,fontWeight: FontWeight.bold),),
+      ) ,
+    );
+  }
+}

74b92cf1a92b4bc95d1eeb47f78ca400aa7a560e
Mahmoud Abdelaty <126201317+Mahmoud-Abdelaty@users.noreply.github.com>
2024-01-10 10:05:04 +0200

    Revert "Initial Commit"
    This reverts commit d8d655cde3d210ba6791161b9a25f4cea5599400.

--- lib/features/onboarding/cubit/onboarding_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'onboarding_cubit.dart';
-
-@immutable
-abstract class OnboardingState {}
-
-class OnboardingInitialState extends OnboardingState {}
-
-class OnboardingNextState extends OnboardingState {}

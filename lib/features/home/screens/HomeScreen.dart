38acc61b608f1e5d91b32326886621979cfbe949
Mahmoud Abdelaty <126201317+Mahmoud-Abdelaty@users.noreply.github.com>
2024-01-16 21:35:22 +0200

    Update Finally Home Screen
    Removing Continue Button and Movies

--- lib/features/home/screens/HomeScreen.dart
+++ lib/features/home/screens/HomeScreen.dart
@@ -4,7 +4,6 @@ import 'package:flutter_project/core/utils/app_text_style.dart';
 import 'package:flutter_project/features/home/screens/widget/latest_movies.dart';
 import 'package:flutter_project/features/home/screens/widget/sliver_app_bar_home.dart';
 import 'package:flutter_project/features/home/screens/widget/trending_daily_movies.dart';
-import 'package:flutter_project/features/home/screens/widget/watching.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -18,7 +17,7 @@ class HomeScreen extends StatelessWidget {
           length: 3,
           child: NestedScrollView(
             headerSliverBuilder: (context, innerBoxIsScrolled) {
-              return [SliverAppbarHome()];
+              return [const SliverAppbarHome()];
             },
             body: CustomScrollView(
               slivers: [
@@ -27,7 +26,9 @@ class HomeScreen extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       const TrendingDailyScreen(),
-                      const WatchingMovies(),
+                      const SizedBox(
+                        height: 15,
+                      ),
                       Padding(
                         padding: const EdgeInsets.only(left: 20),
                         child: Text(
@@ -37,6 +38,9 @@ class HomeScreen extends StatelessWidget {
                             fontSize: 25,
                           ),
                         ),
+                      ),
+                      const SizedBox(
+                        height: 15,
                       ),
                       const LatestMovies(),
                     ],

63822f8dcf1824f6191d74a70ce122b366f24068
Kareem mohamed <info.kareemmohamed@gmail.com>
2024-01-27 10:42:57 +0200

    update Favourite Screen

--- lib/features/favourite/favourite_screen.dart
+++ lib/features/favourite/favourite_screen.dart
@@ -22,23 +22,54 @@ class FavouriteScreen extends StatelessWidget {
           padding: const EdgeInsets.all(8.0),
           child: Row(
             children: [
-              Column(
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   CircleAvatar(
                     radius: 50,
                     backgroundImage: NetworkImage(
-                      'https://image.tmdb.org/t/p/w500/gwsrtzSrr7G6xjxsa7IwaAMHpEF.jpg',
-                    ),
+                        'https://resizing.flixster.com/dV1vfa4w_dB4wzk7A_VzThWUWw8=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzEyZDMyYjZmLThmNzAtNDliNC1hMjFmLTA2ZWY4M2UyMjJhMi5qcGc='),
                   ),
-                  const SizedBox(height: 5 ),
-                  Text(
-                    'Your Text Here',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
+                  SizedBox(
+                    width: 20,
                   ),
+                  Column(children: [
+                    Text(
+                      'oppenheimer',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 5,),
+                    Text(
+                      '2023 Total of 4,25GB',
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 5,),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 100),
+                      child: Text(
+                        'Movie',
+                        style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ]),
+                  SizedBox(width: 0,),
+                  Column(
+                    children: [
+                      ElevatedButton.icon( style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(Colors.white)),onPressed: (){}, icon:Icon( Icons.play_arrow_outlined,color: Colors.black,),label: Text('Show',style: TextStyle(color: Colors.black,fontSize: 13),),),                    ElevatedButton.icon( style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(Colors.red)),onPressed: (){}, icon:Icon( Icons.delete,color: Colors.white,),label: Text('Delete',style: TextStyle(color: Colors.white,fontSize: 13),),)
+                    ],
+                  )
                 ],
               ),
 
